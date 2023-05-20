8115301997d8adeed9560562d6e6e796faa56123
windows10 <AnaCatalinaCab>
2023-05-20 16:04:12 -0300

    Creacion de paquetes: controller y model. Creacion de las clases: servicio, sucursal, producto y noticia

---
+++ TP2_Grupo45/src/main/java/ar/edu/unju/fi/controller/Controlador.java
@@ -0,0 +1,8 @@
+package ar.edu.unju.fi.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class Controlador {
+	
+	
+}
