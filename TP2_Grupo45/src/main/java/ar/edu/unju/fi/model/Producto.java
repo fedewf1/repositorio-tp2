036b263a54ba48efa1de3084452192d707ec2017
joelrojas95 <VOSE@DESKTOP-NCIRM7I>
2023-05-20 19:00:38 -0300

    ISSUE3: En la clase producto se considera los atributos: nombre, codigo, precio, categoria y descuento. En la misma se agregan los metodos accesores (getters y setters). Se agrega un constructor parametizado. Se agrega un metodo calcularDescuento, que retorna el precio con descuento (si tuviera descuento). ISSUE 4: Se crea un nuevo archivo, nuevo_producto.html en la carpeta "templates". Se incluye una base de diseño de formulario.

--- TP2_Grupo45/src/main/java/ar/edu/unju/fi/model/Producto.java
+++ TP2_Grupo45/src/main/java/ar/edu/unju/fi/model/Producto.java
@@ -1,5 +1,72 @@
 package ar.edu.unju.fi.model;
 
 public class Producto {
+	// Atributos
+	private String nombre;
+    private int codigo;
+    private double precio;
+    private String categoria;
+    private int descuento;
 
+    // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(int descuento) {
+        this.descuento = descuento;
+    }
+    
+    // Constructor parametrizado
+    public Producto(String nombre, int codigo, double precio, String categoria, int descuento) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.precio = precio;
+        this.categoria = categoria;
+        this.descuento = descuento;
+    }
+
+    // Método para calcular el descuento y obtener el precio con descuento
+    public double calcularDescuento() {
+        if (descuento > 0 && descuento <= 50) {
+            double porcentajeDescuento = descuento / 100.0;
+            double descuentoAplicado = precio * porcentajeDescuento;
+            return precio - descuentoAplicado;
+        } else {
+            return precio;
+        }
+    }
+    
 }
