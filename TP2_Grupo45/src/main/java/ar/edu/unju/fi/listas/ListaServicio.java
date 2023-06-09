e357cb2f8aa51d5144a9253e0a97602b1e52a132
Jonathan R. Mascareño <jonatanrm92@gmail.com>
2023-06-08 21:40:12 -0300

    Se agrega y/o modifica ServicioController,HTMLs,estilos

---
+++ TP2_Grupo45/src/main/java/ar/edu/unju/fi/listas/ListaServicio.java
@@ -0,0 +1,29 @@
+package ar.edu.unju.fi.listas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import org.springframework.stereotype.Component;
+
+import ar.edu.unju.fi.model.Servicio;
+@Component
+public class ListaServicio {
+
+	private List<Servicio> servicios;
+
+	public ListaServicio() {
+		servicios = new ArrayList<Servicio>();
+
+	}
+
+	public List<Servicio> getServicios() {
+		return servicios;
+	}
+
+	public void setServicios(List<Servicio> servicios) {
+		this.servicios = servicios;
+	}
+	
+	
+}
