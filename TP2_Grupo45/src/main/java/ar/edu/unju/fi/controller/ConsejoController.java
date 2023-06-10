1b7c72993b59fb664d155741e6ce12919ba5dfe8
Jonathan R. Mascareño <jonatanrm92@gmail.com>
2023-06-09 22:10:43 -0300

    Modificaciones header,contacto

--- TP2_Grupo45/src/main/java/ar/edu/unju/fi/controller/ConsejoController.java
+++ TP2_Grupo45/src/main/java/ar/edu/unju/fi/controller/ConsejoController.java
@@ -1,5 +1,7 @@
 package ar.edu.unju.fi.controller;
 
+//import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,64 +11,67 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-import ar.edu.unju.fi.listas.ListaConsejo;
+
 import ar.edu.unju.fi.model.Consejo;
+import ar.edu.unju.fi.service.IConsejoService;
 import jakarta.validation.Valid;
 
 
 
 @Controller
 public class ConsejoController {
+
+	@Autowired
+	private IConsejoService consejoService;
+	
 	
 	@Autowired
 	private Consejo consejo;
 
-	@Autowired
-	private ListaConsejo listaConsejos;
 
-	
 	//Muestra la lista de consejos
 	@GetMapping("/consejos")
-	public String mostrarListaConsejos(Model model) {
-		model.addAttribute("consejos", listaConsejos.getConsejos());
-		return "consejos";
+	public ModelAndView getConsejosPage() {
+			ModelAndView mav = new ModelAndView("consejos");
+			mav.addObject("consejos", consejoService.getConsejos());
+			return mav;
 	}
 
 	//Muestra en un html, todo el contenido de un consejo a partir de su id
 	@GetMapping("/consejo/detallado/{id}")
 	public String mostrarDetalleConsejo(@PathVariable("id") int id, Model model) {
-	    Consejo consejo = listaConsejos.findConsejoById(id);
+	    Consejo consejo = consejoService.findConsejoById(id);
 	    model.addAttribute("consejo", consejo);
 	    return "detalle_consejo";
 	}
 	
 	
-	//Muestra el formulario para nuevo consejo
-	@GetMapping("/consejo/nuevo")
-	public String mostrarFormularioNuevoServicio(Model model) {
+
+	@GetMapping("consejo/nuevo")
+	public ModelAndView getFormularioNuevoConsejo() {
+		consejo = new Consejo();
 		boolean editando = false;
-
-		model.addAttribute("consejo", consejo);
-		model.addAttribute("editando", editando);
-
-		return "nuevo_consejo";
+		ModelAndView mav = new ModelAndView("nuevo_consejo");
+		mav.addObject("consejo", consejo);
+		mav.addObject("editando", editando);
+		return mav;
+		
 	}
+	
 
 	//Solicitud para guardar lo rellenado en un formulario
 	@PostMapping("/consejo/guardar")
 	public ModelAndView getGuardarConsejoPage(@Valid @ModelAttribute("consejo") Consejo consejo, BindingResult result) {
+	    if (result.hasErrors()) {
+	        ModelAndView modelView = new ModelAndView("nuevo_consejo");
+	        modelView.addObject("consejos", consejoService.getConsejos());
+	        return modelView;
+	    }
 
-		if (result.hasErrors()) {
-			ModelAndView modelView = new ModelAndView("nuevo_consejo");
-			modelView.addObject("consejo", consejo);
-			return modelView;
-		}
-
-		ModelAndView modelAndView = new ModelAndView("consejos");
-		
-		listaConsejos.agregarConsejo(consejo);
-		modelAndView.addObject("consejos", listaConsejos.getConsejos());
-		return modelAndView;
+	    ModelAndView modelAndView = new ModelAndView("consejos");
+	    consejoService.guardarConsejo(consejo); 
+	    modelAndView.addObject("consejos", consejoService.getConsejos());
+	    return modelAndView;
 	}
 
 	
@@ -74,7 +79,7 @@ public class ConsejoController {
 	@GetMapping("consejo/modificar/{id}")
 	public String mostrarFormularioEdicion(@PathVariable("id") Integer id, Model model) {
 		boolean editando = true;
-		Consejo consejo = listaConsejos.findConsejoById(id);
+		Consejo consejo = consejoService.findConsejoById(id);
 		
 		model.addAttribute("consejo", consejo);
 		model.addAttribute("editando", editando);
@@ -83,22 +88,16 @@ public class ConsejoController {
 	//Solicita la modificación de un consejo a traves de su id
 	@PostMapping("/consejo/modificar/{id}")
 	public String editarConsejo(@PathVariable("id") Integer id, @ModelAttribute("consejo") Consejo consejoModificado) {
-		
-		Consejo consejo = listaConsejos.findConsejoById(id);
-		consejo.setTitulo(consejoModificado.getTitulo());
-		consejo.setAutor(consejoModificado.getAutor());
-		consejo.setContenido(consejoModificado.getContenido());
+	    consejoService.actualizarConsejo(id, consejoModificado);
 
-		return "redirect:/consejos";
+	    return "redirect:/consejos";
 	}
 
+	//Elimina un consejo tomando como base un id
 	@GetMapping("/consejo/eliminar/{id}")
-	public String eliminarConsejo(Model model, @PathVariable(value = "id") Integer id) {
-
-		listaConsejos.eliminarConsejo(id);
-		listaConsejos.actualizarId();
-
-		return "redirect:/consejos";
+	public String eliminarConsejo(@PathVariable(value = "id") Integer id) {
+	    consejoService.consejoAEliminar(id);
+	    return "redirect:/consejos";
 	}
  
 }
--- TP2_Grupo45/src/main/java/ar/edu/unju/fi/service/IConsejoService.java
+++ TP2_Grupo45/src/main/java/ar/edu/unju/fi/service/IConsejoService.java
@@ -1,5 +1,20 @@
 package ar.edu.unju.fi.service;
 
-public interface IConsejoService {
+import java.util.List;
 
+import ar.edu.unju.fi.model.Consejo;
+
+public interface IConsejoService {
+	
+	List<Consejo> getConsejos();
+	Consejo getConsejo();
+
+	void agregarConsejo(Consejo consejo);
+	Consejo findConsejoById(Integer id);
+	void actualizarId(Integer id);
+	void consejoAEliminar(Integer id);
+	void actualizarConsejo(Integer id, Consejo consejoModificado);
+	
+	void guardarConsejo(Consejo consejo);
+	
 }
